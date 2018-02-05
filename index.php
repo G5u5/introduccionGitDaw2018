9457b79e0598065e780511caea3d61e4f2b97d2f
jdurantez <jdurantez@I1108>
2018-02-05 10:48:37 +0100

    primer commit

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <h1>Proyecto php para git</h1>
+        <p>Proyecto para probar los comandos de git y los menús de NetBeans</p>
+    </body>
+</html>
